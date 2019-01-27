dd91e3739bd0737e53df151afe8a76955eed9846
George Plymale II <georgedp@orbitalimpact.com>
2019-01-27 17:05:52 -0500

    upgrade to libpipeline 1.5.1 and remove its patch

--- Formula/libpipeline.rb
+++ Formula/libpipeline.rb
@@ -1,25 +1,10 @@
 class Libpipeline < Formula
   desc "C library for manipulating pipelines of subprocesses"
   homepage "http://libpipeline.nongnu.org/"
-  url "https://download.savannah.nongnu.org/releases/libpipeline/libpipeline-1.5.0.tar.gz"
-  sha256 "0d72e12e4f2afff67fd7b9df0a24d7ba42b5a7c9211ac5b3dcccc5cd8b286f2b"
+  url "https://download.savannah.nongnu.org/releases/libpipeline/libpipeline-1.5.1.tar.gz"
+  sha256 "d633706b7d845f08b42bc66ddbe845d57e726bf89298e2cee29f09577e2f902f"
 
   depends_on "pkg-config"
-
-  # Patch explanation:
-  # Gnulib, part of man-db and libpipeline, externs a symbol called
-  # `program_name`. This symbol is supposed to be defined by glibc, which is to
-  # be expected on Linux. However, since we're on OS X, we don't have glibc and
-  # so this symbol will not be defined. This is a known issue and several work-
-  # arounds have been proposed by different people. One option is to use some
-  # CFLAGS to suppress errors about undefined symbols. Unfortunately, this only
-  # seems to work for man-db and doesn't work for other programs which link with
-  # libpipeline. This patch is a more general solution and was almost included
-  # in the Nix package manager at one point. For more info, visit these links:
-  # https://stackoverflow.com/questions/36824434/libpipeline-fails-to-compile-on-mac-os-x
-  # https://github.com/NixOS/nixpkgs/issues/15849
-  # https://lists.gnu.org/archive/html/bug-gnulib/2015-02/msg00079.html
-  patch :DATA
 
   def install
     ENV["CC"] = "#{ENV.cc} -arch x86_64"
@@ -45,23 +30,3 @@ class Libpipeline < Formula
     system "./test"
   end
 end
-
-__END__
-diff --git a/gnulib/lib/error.c b/gnulib/lib/error.c
-index 31109df3..6b68cd15 100644
---- a/gnulib/lib/error.c
-+++ b/gnulib/lib/error.c
-@@ -110,9 +110,13 @@ int strerror_r ();
- #  endif
- # endif
-
-+#if defined __APPLE__ && defined __MACH__
-+#define program_name (((char **)*_NSGetArgv())[0])
-+#else
- /* The calling program should define program_name and set it to the
-    name of the executing program.  */
- extern char *program_name;
-+#endif
-
- # if HAVE_STRERROR_R || defined strerror_r
- #  define __strerror_r strerror_r
